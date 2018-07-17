1ee013d774721cc5ed84442c56e4f4ab53ed4d9a
hty6746 <38169253+hty6746@users.noreply.github.com>
2018-07-17 19:55:09 -0400

    Add files via upload
    Fixed the gradients in loss

--- Stein_GAN.py
+++ Stein_GAN.py
@@ -1,3 +1,5 @@
+# I just simply migrated the code from WassersteinGAN here and commented out some lines we don't need.
+
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
@@ -5,86 +7,150 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import os
 
+DIR = os.getcwd() + "/output/"
+EXP = "071619-1"
+EXP_DIR = DIR + EXP + "/"
+if not os.path.exists(EXP_DIR):
+    os.makedirs(EXP_DIR)
 
 mb_size = 100
-X_dim = 3  # dimension of the target distribution, 3 for e.g.
-z_dim = 5
-h_dim = 20
-
+X_dim = 2  # dimension of the target distribution, 3 for e.g.
+z_dim = 10
+h_dim_g = 50
+h_dim_d = 50
+N, n_D, n_G = 50000, 1, 1  # num of iterations
 
 # As a simple example, use a 3-d Gaussian as target distribution
-# Parameters for the target Gaussian distribution
-mu = np.array([2, 3, 5], dtype=np.float32)
-Sigma = np.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=np.float32)
-Sigma_inv = np.linalg.inv(Sigma)
-# Parameters for the second Gaussian distribution and weights if consider a Gaussian mixture
-mu_2 = np.array([-2, -3, -5], dtype=np.float32)
-Sigma_2 = np.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=np.float32)
-Sigma_inv_2 = np.linalg.inv(Sigma_2)
-w_1 = w_2 = 0.5
+
+# parameters
+p1, p2 = 0.5, 0.5
+# mu1, mu2 = np.array([2, 2, 2]), np.array([-1, -1, -1])
+# Sigma1 = Sigma2 = np.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+mu1, mu2 = np.array([2, 2]), np.array([-1, -1])
+Sigma1 = Sigma2 = np.matrix([[1, 0], [0, 1]])
+Sigma1_inv = np.linalg.inv(Sigma1)
+Sigma2_inv = np.linalg.inv(Sigma2)
+Sigma1_det = np.linalg.det(Sigma1)
+Sigma2_det = np.linalg.det(Sigma2)
 
 
-# Define initializer
-initializer = tf.contrib.layers.xavier_initializer()
+################################################################################################
+################################################################################################
+
+info = open(EXP_DIR + "info.txt", 'w')
+info.write("Description: " +
+           '\n\n' + ("=" * 80 + '\n') * 3 + '\n' +
+           "Model Parameters: \n\t" +
+           "\n\t".join(['p1 = {}'.format(p1), 'p2 = {}'.format(p2),
+                        'mu1 = {}'.format(mu1), 'mu2 = {}'.format(mu1),
+                        'sigma1 = {}'.format(Sigma1), 'sigma2 = {}'.format(Sigma2)]) +
+           '\n\n' + ("=" * 80 + '\n') * 3 + '\n' +
+           "Network Parameters: \n\t" +
+           "\n\t".join(['mb_size = {}'.format(mb_size), 'X_dim = {}'.format(X_dim), 'z_dim = {}'.format(z_dim),
+                        'h_dim_g = {}'.format(h_dim_g), 'h_dim_d = {}'.format(h_dim_d), 'n_D = {}'.format(n_D),
+                        'n_G = {}'.format(n_G)]) +
+           '\n\n' + ("=" * 80 + '\n') * 3 + '\n' +
+           "Additional Information: \n" +
+           "" + "\n")
+info.close()
 
 
-# Define variables in the discriminator
+################################################################################################
+################################################################################################
+
+# plot the contour of the mixture on the first 2 dimensions
+X1, X2 = np.meshgrid(np.linspace(-10., 10.), np.linspace(-10., 10.))
+XX = np.array([X1.ravel(), X2.ravel()]).T
+Y = (p1 * np.exp(-np.diag(np.matmul(np.matmul(XX - mu1[:2], Sigma1_inv[:2, :2]), (XX - mu1[:2]).T)) / 2) +
+     p2 * np.exp(-np.diag(np.matmul(np.matmul(XX - mu2[:2], Sigma2_inv[:2, :2]), (XX - mu2[:2]).T)) / 2))
+Y = Y.reshape(X1.shape)
+CS = plt.contour(X1, X2, Y)
+CB = plt.colorbar(CS, shrink=0.8, extend='both')
+plt.title("Contour plot of the target distribution")
+plt.savefig(EXP_DIR + "target.png", format="png")
+plt.close()
+
+
+# convert parameters to tf tensor
+mu1_tf = tf.convert_to_tensor(mu1, dtype=tf.float32)
+mu2_tf = tf.convert_to_tensor(mu2, dtype=tf.float32)
+Sigma1_inv_tf = tf.convert_to_tensor(Sigma1_inv, dtype=tf.float32)
+Sigma2_inv_tf = tf.convert_to_tensor(Sigma2_inv, dtype=tf.float32)
+
+
 X = tf.placeholder(tf.float32, shape=[None, X_dim])
 
-D_W1 = tf.get_variable('D_w1', [X_dim, h_dim], dtype=tf.float32, initializer=initializer)
-D_b1 = tf.get_variable('D_b1', [h_dim], initializer=initializer)
-D_W2 = tf.get_variable('D_w2', [h_dim, 1], dtype=tf.float32, initializer=initializer)
-D_b2 = tf.get_variable('D_b2', [1], initializer=initializer)
+initializer = tf.contrib.layers.xavier_initializer()
+
+D_W1 = tf.get_variable('D_w1', [X_dim, h_dim_d], dtype=tf.float32, initializer=initializer)
+D_b1 = tf.get_variable('D_b1', [h_dim_d], initializer=initializer)
+D_W2 = tf.get_variable('D_w2', [h_dim_d, X_dim], dtype=tf.float32, initializer=initializer)
+D_b2 = tf.get_variable('D_b2', [X_dim], initializer=initializer)
 
 theta_D = [D_W1, D_W2, D_b1, D_b2]
 
 
-# Define variables in the generator
 z = tf.placeholder(tf.float32, shape=[None, z_dim])
 
-G_W1 = tf.get_variable('g_w1', [z_dim, h_dim], dtype=tf.float32, initializer=initializer)
-G_b1 = tf.get_variable('g_b1', [h_dim], initializer=initializer)
-G_W2 = tf.get_variable('g_w2', [h_dim, X_dim], dtype=tf.float32, initializer=initializer)
+G_W1 = tf.get_variable('g_w1', [z_dim, h_dim_g], dtype=tf.float32, initializer=initializer)
+G_b1 = tf.get_variable('g_b1', [h_dim_g], initializer=initializer)
+G_W2 = tf.get_variable('g_w2', [h_dim_g, X_dim], dtype=tf.float32, initializer=initializer)
 G_b2 = tf.get_variable('g_b2', [X_dim], initializer=initializer)
 
 theta_G = [G_W1, G_W2, G_b1, G_b2]
 
 
-# Score function computed from the target distribution
-# def density(x):
-#     return tf.exp(-tf.matmul(tf.matmul(x-mu, Sigma_inv), tf.transpose(x-mu))/2)
+# # target log-density
+# def log_density(x):
+#     def density(x):
+#         return tf.exp(-tf.matmul(tf.matmul(x - mu_tf, Sigma_inv_tf), tf.transpose(x - mu_tf)) / 2)
+#     # return -tf.matmul(tf.matmul(x - mu_tf, Sigma_inv_tf), tf.transpose(x - mu_tf)) / 2
+#     # return tf.reduce_logsumexp(-tf.matmul(tf.matmul(x - mu_tf, Sigma_inv_tf), tf.transpose(x - mu_tf))/2)
+#     # return tf.log(density(x))
+#     log_den1 = -tf.matmul(tf.matmul(x - mu1_tf, Sigma1_inv_tf), tf.transpose(x - mu1_tf)) / 2
+#     log_den2 = -tf.matmul(tf.matmul(x - mu2_tf, Sigma2_inv_tf), tf.transpose(x - mu2_tf)) / 2
+#     return tf.reduce_logsumexp(tf.concat([log_p1_tf + log_den1,
+#                                           log_p2_tf + log_den2], 0))
+
+
+# log densities for a collection of samples (G_sample)
+def log_densities(xs):
+    log_den1 = - tf.diag_part(tf.matmul(tf.matmul(xs - mu1_tf, Sigma1_inv_tf),
+                                        tf.transpose(xs - mu1_tf))) / 2 - np.log(Sigma1_det) / 2
+    log_den2 = - tf.diag_part(tf.matmul(tf.matmul(xs - mu2_tf, Sigma2_inv_tf),
+                                        tf.transpose(xs - mu2_tf))) / 2 - np.log(Sigma2_det) / 2
+    return tf.expand_dims(tf.reduce_logsumexp(tf.stack([np.log(p1) + log_den1,
+                                                        np.log(p2) + log_den2], 0), 0), 1)
+    # return log_den1
 
 
 # Score function computed from the target distribution
-def S_q(x):
-    return tf.matmul(mu - x, Sigma_inv)
+def S_q(xs):
+    # return tf.matmul(mu_tf - x, Sigma_inv_tf)
+    return tf.gradients(log_densities(xs), xs)[0]
 
 
-'''
-# The score function of a Gaussian mixture
-# Using tf.reduce_logsumexp instead of tf.log(tf.exp(...)) to avoid overflow (NaN produced)
-def S_q(x):
-    d_1 = -tf.matmul(tf.matmul(x-mu, Sigma_inv), tf.transpose(x-mu))/2 + tf.log(w_1)
-    d_2 = -tf.matmul(tf.matmul(x-mu_2, Sigma_inv_2), tf.transpose(x-mu_2))/2 + tf.log(w_2)
-    log_d = tf.reduce_logsumexp([d_1, d_2])
-    return tf.gradients(log_d, x)
-'''
-
-
-def sample_z(m, n):
-    return np.random.uniform(-10., 10., size=[m, n])
+def sample_z(m, n, bound=1.):
+    np.random.seed(1)
+    return np.random.uniform(-bound, bound, size=[m, n])
 
 
 def generator(z):
     G_h1 = tf.nn.relu(tf.matmul(z, G_W1) + G_b1)
-    out = tf.matmul(G_h1, G_W2) + G_b2
+    out = (tf.matmul(G_h1, G_W2) + G_b2)
     return out
 
 
+# output dimension of this function is X_dim
 def discriminator(x):
     D_h1 = tf.nn.relu(tf.matmul(x, D_W1) + D_b1)
     out = tf.nn.tanh(tf.matmul(D_h1, D_W2) + D_b2)
     return out
+
+
+def diag_gradient(y, x):
+    dg = tf.stack([tf.gradients(y[:, i], x)[0][:, i] for i in range(X_dim)], axis=0)
+    return tf.transpose(dg)
 
 
 G_sample = generator(z)
@@ -94,7 +160,9 @@ D_fake = discriminator(G_sample)
 # D_loss = tf.reduce_mean(D_real) - tf.reduce_mean(D_fake)
 # G_loss = -tf.reduce_mean(D_fake)
 
-Loss = tf.reduce_sum(tf.square(S_q(G_sample) * D_fake + tf.gradients(D_fake, G_sample)))
+
+Loss = tf.reduce_sum(tf.square(tf.multiply(S_q(G_sample), D_fake) + diag_gradient(D_fake, G_sample)))
+
 
 # D_solver = (tf.train.RMSPropOptimizer(learning_rate=1e-4)
 #             .minimize(-D_loss, var_list=theta_D))
@@ -106,26 +174,79 @@ D_solver = (tf.train.AdamOptimizer(learning_rate=1e-4)
 G_solver = (tf.train.AdamOptimizer(learning_rate=1e-4)
             .minimize(Loss, var_list=theta_G))
 
-
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
-
-# print(sess.run(tf.gradients(D_fake, G_sample), feed_dict={z: sample_z(1, z_dim)}))
+# print(sess.run(tf.gradients(D_fake, G_sample), feed_dict={z: sample_z(mb_size, z_dim)}))
 # print(D_W1.eval(session=sess))
 
-for it in range(10000):
-    for _ in range(5):
+
+D_loss = np.zeros(N)
+G_loss = np.zeros(N)
+for it in range(N):
+    for _ in range(n_D):
         _, D_Loss_curr = sess.run([D_solver, Loss],
                                   feed_dict={z: sample_z(mb_size, z_dim)})
+    D_loss[it] = D_Loss_curr
 
-    for _ in range(5):
+    for _ in range(n_G):
         _, G_Loss_curr = sess.run([G_solver, Loss],
                                   feed_dict={z: sample_z(mb_size, z_dim)})
+    G_loss[it] = G_Loss_curr
 
     if it % 100 == 0:
-        samples = sess.run(G_sample, feed_dict={z: sample_z(100, z_dim)})
+        samples = sess.run(G_sample, feed_dict={z: sample_z(mb_size, z_dim)})
         print(np.mean(samples, axis=0))
         print("D_loss", it, ":", D_Loss_curr)
         print("G_loss", it, ":", G_Loss_curr)
-
+        plt.scatter(samples[:, 0], samples[:, 1], color='b')
+        plt.scatter([mu1[0], mu2[0]], [mu1[1], mu2[1]], color="r")
+        plt.title("Samples at iter {0:04d}, with loss {{D: {1:.4f}, G: {2:.4f}}}.".format(it, D_Loss_curr, G_Loss_curr))
+        plt.savefig(EXP_DIR + "iter {0:04d}".format(it))
+        plt.close()
+        # sqs= sess.run(S_q_s(G_sample), feed_dict={z: sample_z(mb_size, z_dim)})
+        # print(it)
+        # print(sqs)
+        # print(sq1, '\n', sq2, '\n', sq3)
+        # print(np.sum(sq1, 0))
+        # print(sq_g_samples)
 sess.close()
+
+
+np.savetxt(EXP_DIR + "loss_D.csv", D_loss, delimiter=",")
+plt.plot(D_loss)
+plt.ylim(ymin=0)
+plt.axvline(np.argmin(D_loss), ymax=np.min(D_loss), color="r")
+plt.title("loss_D (min at iter {})".format(np.argmin(D_loss)))
+plt.savefig(EXP_DIR + "loss_D.png", format="png")
+plt.close()
+
+np.savetxt(EXP_DIR + "loss_G.csv", G_loss, delimiter=",")
+plt.plot(G_loss)
+plt.ylim(ymin=0)
+plt.axvline(np.argmin(G_loss), ymax=np.min(G_loss), color="r")
+plt.title("loss_G (min at iter {})".format(np.argmin(G_loss)))
+plt.savefig(EXP_DIR + "loss_G.png", format="png")
+plt.close()
+
+
+# for it in range(2000):
+#     for _ in range(5):
+#         X_mb, _ = mnist.train.next_batch(mb_size)
+#         _, D_loss_curr, _ = sess.run(
+#             [D_solver, D_loss, clip_D],
+#             feed_dict={X: X_mb, z: sample_z(mb_size, z_dim)}
+#         )
+#     _, G_loss_curr = sess.run(
+#         [G_solver, G_loss],
+#         feed_dict={z: sample_z(mb_size, z_dim)}
+#     )
+#     if it % 100 == 0:
+#         print('Iter: {}; D loss: {:.4}; G_loss: {:.4}'
+#               .format(it, D_loss_curr, G_loss_curr))
+#         if it % 1000 == 0:
+#             samples = sess.run(G_sample, feed_dict={z: sample_z(16, z_dim)})
+#             fig = plot(samples)
+#             plt.savefig('out/{}.png'
+#                         .format(str(i).zfill(3)), bbox_inches='tight')
+#             i += 1
+#             plt.close(fig)
